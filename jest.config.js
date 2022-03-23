549c2d0e1727dcca3ea93bccd99d63bca789a800
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-03-23 19:04:06 +0100

    Set up jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleFileExtensions: [
+    "js",
+    "ts",
+    "json",
+    "svelte"
+  ],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  transform: {
+    "^.+\\.js$": "ts-jest",
+    "^.+\\.ts": "ts-jest",
+    "^.+\\.svelte$": ["svelte-jester", { "preprocess": true }],
+  },
+};
---
+++ test/lib/App.spec.ts
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import { render } from "@testing-library/svelte";
+import App from "../../src/App.svelte";
+
+describe("App component", () => {
+	test("has a link", () => {
+		const { getByTestId } = render(App);
+		expect(getByTestId("some-link")).toHaveAttribute('href', 'https://svelte.dev/tutorial')
+	});
+});
